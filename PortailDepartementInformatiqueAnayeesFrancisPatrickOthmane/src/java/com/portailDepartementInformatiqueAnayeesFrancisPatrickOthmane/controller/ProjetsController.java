81652638c073f75eb72efc44762d3918f722b429
Anayees Sarkes <1428398@crosemont.qc.ca>
2023-03-26 15:39:40 -0400

    web.xml

--- PortailDepartementInformatiqueAnayeesFrancisPatrickOthmane/src/java/com/portailDepartementInformatiqueAnayeesFrancisPatrickOthmane/controller/ProjetsController.java
+++ PortailDepartementInformatiqueAnayeesFrancisPatrickOthmane/src/java/com/portailDepartementInformatiqueAnayeesFrancisPatrickOthmane/controller/ProjetsController.java
@@ -7,7 +7,6 @@ package com.portailDepartementInformatiqueAnayeesFrancisPatrickOthmane.controlle
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +15,6 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Anayees
  */
-@WebServlet(name = "ProjetsController", urlPatterns = {"/ProjetsController"})
 public class ProjetsController extends HttpServlet {
 
     /**
