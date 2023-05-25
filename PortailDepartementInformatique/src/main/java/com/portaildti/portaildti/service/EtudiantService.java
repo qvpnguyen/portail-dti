bd1585aee00c1860173c83993ed6ebe498963f7c
Patrick Nguyen <patrick@lienmultimedia.com>
2023-05-25 11:12:00 -0400

    Regler des bugs

--- PortailDepartementInformatique/src/main/java/com/portaildti/portaildti/service/EtudiantService.java
+++ PortailDepartementInformatique/src/main/java/com/portaildti/portaildti/service/EtudiantService.java
@@ -162,4 +162,6 @@ public class EtudiantService {
     public void updateActiveStatus(Integer id, boolean enabled) {
         repo.updateActiveStatusEtudiant(id, enabled);
     }
+    public List<Etudiant> rechercherEtudiantsTuteur() {
+        return (List<Etudiant>) repo.findEtudiantsTuteurs();    }
 }
